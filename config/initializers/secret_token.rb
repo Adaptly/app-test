4f615ec7c3dded52e0b67b232b9d36b28247dbed
Philip Champon <philip@adaptly.com>
2014-04-09 14:02:01 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AppTest::Application.config.secret_key_base = 'c00b7abfef57268000ee14330dfefd3189c37edc30113ff662cee7f46f2f19a8e3717e72546302ec91f1c9e5397506ee9668eed6d77fc5a89ad3d098ac14df8e'
